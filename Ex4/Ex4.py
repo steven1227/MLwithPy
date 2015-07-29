13af7f02471e3d1469a125213be89235574012c8
Rendong Liu <rendong_liu@hotmail.com>
2015-07-29 22:14:36 +0200

    give the cost function

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -56,11 +56,43 @@ def paramUnroll(nn_params, input_layer_size, hidden_layer_size,
     return (theta1, theta2)
 
 
+def sigmoid(z):
+    return scipy.special.expit(z)
+
+
+def recodeLabel(y, k):
+    m = shape(y)[0]
+    out = zeros((k, m))
+    for i in range(0, m):
+        out[y[i] - 1, i] = 1
+    return out
+
+
+def feedForward(theta1, theta2, X, X_bias):
+    one_rows = ones((1, shape(X)[0]))
+    a1 = r_[one_rows, X.T] if X_bias is None else X_bias
+    z2 = theta1.dot(a1)
+    a2 = sigmoid(z2)
+    a2 = r_[one_rows, a2]
+    z3 = theta2.dot(a2)
+    a3 = sigmoid(z3)
+    return (a1, a2, a3, z2, z3)
+
+
 def computeCost(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lamda, yk=None, X_bias=None):
     m, n = shape(X)
     theta1, theta2 = paramUnroll(nn_params, input_layer_size, hidden_layer_size, num_labels)
+    a1, a2, a3, z2, z3 = feedForward(theta1, theta2, X, X_bias)
+
+    if yk is None:
+        yk = recodeLabel(y, num_labels)
+        assert shape(yk) == shape(a3), "Error, shape of recoded y is different from a3"
     print theta1.shape, theta2.shape
-    return None
+    term1 		= -yk * log( a3 )
+    term2 		= (1 - yk) * log( 1 - a3 )
+    left_term 	= sum(term1 - term2) / m
+    right_term 	= sum(theta1[:,1:] ** 2) + sum(theta2[:,1:] ** 2)
+    return left_term + right_term * lamda / (2 * m)
 
 
 mat = scipy.io.loadmat(os.path.dirname(os.path.realpath(__file__)) + '/ex4data1.mat')
@@ -70,7 +102,7 @@ theta1, theta2 = mat2['Theta1'], mat2['Theta2']
 input_layer_size = 400
 hidden_layer_size = 25
 num_labels = 10
-lamda = 0
+lamda = 1
 displayData(X)
 
 # a = array([[10, 20], [30, 40]])
@@ -78,5 +110,6 @@ displayData(X)
 # print r_[a.flatten(), b.flatten()]
 params = r_[theta1.T.flatten(), theta2.T.flatten()]
 print theta1.shape, theta2.shape
+# print X.shape
 print params.shape
 print computeCost(params, input_layer_size, hidden_layer_size, num_labels, X, Y, lamda)
