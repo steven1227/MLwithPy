730b049d496a4006b92242e6b78bc415b57ae190
Rendong Liu <rendong_liu@hotmail.com>
2015-08-11 21:43:23 +0200

    finish kmeans plot

--- Ex7/Ex7_KM.py
+++ Ex7/Ex7_KM.py
@@ -26,13 +26,31 @@ def ComputeCentroids(x, idx, K):
         center_new[i] = [np.sum(temp[:,0])/temp.shape[0],np.sum(temp[:,1])/temp.shape[0]]
     return center_new
 
+def runkMeans(X, initial_centroids, max_iters, plot=False ):
+    for iteration in range( 0, max_iters ):
+        idx =findClosetCentroid(X,initial_centroids)
+        initial_centroids = ComputeCentroids(X, idx, K)
+        pyplot.plot(initial_centroids[:, 0],initial_centroids[:, 1],'gs', markersize=6)
+
+    if plot == True:
+        temp1 = X[1 == idx[:,0]]
+        temp2 = X[2 == idx[:,0]]
+        temp3 = X[3 == idx[:,0]]
+        print temp1.shape, temp2.shape, temp3.shape
+
+        pyplot.plot(temp1[:, 0], temp1[:, 1], 'bo', markersize=5)
+        pyplot.plot(temp2[:, 0], temp2[:, 1], 'ro', markersize=5)
+        pyplot.plot(temp3[:, 0], temp3[:, 1], 'yo', markersize=5)
+    pass
+
+
 mat = scipy.io.loadmat(os.getcwd() + "/ex7data2.mat")
 X = mat['X']
 K = 3
 
 initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
-
 idx = findClosetCentroid(X, initial_centroids)
 centroids = ComputeCentroids(X, idx, K)
-pyplot.plot(X[:, 0], X[:, 1], 'bx', markersize=5)
+
+runkMeans(X, initial_centroids, 10, plot=True)
 pyplot.show()
